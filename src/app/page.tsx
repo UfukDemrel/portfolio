96e333db59cc7b958eceb5de9e9bf99c728c04a2
UfukDemrel <126552914+UfukDemrel@users.noreply.github.com>
2024-01-11 12:17:18 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,105 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className='flex bg-white'>
+        <div className=' w-2/5 top-0 box-border p-5 h-screen sticky text-black'>
+          <div className=' pt-16 pb-16'>
+            <img className=' w-1/3 rounded-full h-44 object-cover mb-10' src='./demirel.png' alt='alt'/>
+            <div className=' font-semibold text-3xl mb-10'>Ufuk Demirel</div>
+            <div className='flex font-medium'>As a frontend developer, I proficiently utilize technologies such as React, Next.js, Tailwind CSS, CSS, HTML, JavaScript, and jQuery to develop user-friendly and fast web applications. 
+            The combination of my design skills with these technologies allows me to create modern and compelling user interfaces.</div>
+          </div>
+          <div className='flex'>
+            <div className='w-auto h-auto bg-amber-200 flex items-center pt-1 pb-1 pl-2 pr-2 rounded-md mr-5'>
+              <img className='w-5' src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png" alt='alt'/>
+              <div className='pl-3 text-sm'>Gmail</div>
+            </div>
+
+            <div className='w-auto h-auto bg-red-200 flex items-center pt-1 pb-1 pl-2 pr-2 rounded-md'>
+              <img className='w-5' src="https://seeklogo.com/images/A/acrobat-file-pdf-logo-37A1BFDE35-seeklogo.com.png" alt='alt'/>
+              <div className='pl-3 text-sm'>CV</div>
+            </div>
+          </div>
         </div>
+
+        <div className=' flex-1 p-3 box-border text-black pt-16 pb-16'>
+          <div className='flex justify-center'>
+            <div className=' w-32 border-2 h-32 p-3 rounded-xl bg-rose-300 mr-10'>
+              <img className='w-8 pb-3' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png' alt='alt'/>
+              <div className=' text-xs text-white pb-3'>@ufukdemrel</div>
+              <div className=' bg-rose-500 p-1 text-xs rounded-lg w-fit font-medium text-white'>Follow</div>
+            </div>
+            
+            <a target='_blank' href="https://twitter.com/FrontJockey"> 
+            <div className=' w-32 border-2 h-32 p-3 rounded-xl bg-slate-400 mr-10'>
+              <img className='w-8 pb-3' src='https://about.twitter.com/content/dam/about-twitter/x/brand-toolkit/logo-black.png.twimg.1920.png' alt='alt'/>
+              <div className=' text-xs text-white pb-3'>@ufukdemrel</div>
+              <div className=' bg-slate-600 p-1 text-xs rounded-lg w-fit font-medium text-white'>Follow</div>
+            </div>
+            </a>
+
+            <a target='_blank' href="https://www.linkedin.com/in/ufuk-demirel-3136b6183/"> 
+            <div className=' w-32 border-2 h-32 p-3 rounded-xl bg-sky-400 mr-10'>
+              <img className='w-8 pb-3' src='https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png' alt='alt'/>
+              <div className=' text-xs text-white pb-3'>@ufukdemrel</div>
+              <div className=' bg-sky-600 p-1 text-xs rounded-lg w-fit font-medium text-white'>Follow</div>
+            </div>
+            </a>
+            
+            <a target='_blank' href="https://github.com/UfukDemrel"> 
+            <div className=' w-32 border-2 h-32 p-3 rounded-xl bg-slate-700 mr-10'>
+              <img className='w-8 pb-3' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt='alt'/>
+              <div className=' text-xs text-white pb-3'>@ufukdemrel</div>
+              <div className=' bg-slate-600 p-1 text-xs rounded-lg w-fit font-medium text-white'>Follow</div>
+            </div>
+            </a>
+          </div>
+          <div className='flex justify-center p-5'>
+
+          <div className='p-3'>
+            <a className=' p-5' href="https://youtu.be/n8IDqeaPi8U" target='_blank'>
+            <div className='w-96 h-auto bg-rose-200 rounded-xl p-3'>
+              <img className='w-10 pb-2' src='https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png?20220706172052' alt='alt'/>
+              <div className='pb-2 text-xs font-medium'>youtube.com</div>
+              <div className='font-medium text-sm pb-2 whitespace-nowrap w-full overflow-hidden text-ellipsis'>CodeEditor Clone Website - (Next.Js, React, TailwindCSS)</div>
+              <img className='rounded-xl' src="https://i9.ytimg.com/vi/n8IDqeaPi8U/maxresdefault.jpg?v=659ab0f4&sqp=CLjX_qwG&rs=AOn4CLDT2lG4tqa1WeHy1Es9FZlmjdQGwA" alt='alt'/>
+            </div>
+            </a>
+
+            
+            <a className=' p-5' href="https://youtu.be/n8IDqeaPi8U" target='_blank'>
+            <div className='w-96 h-auto bg-rose-200 rounded-xl p-3'>
+              <img className='w-10 pb-2' src='https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png?20220706172052' alt='alt'/>
+              <div className='pb-2 text-xs font-medium'>youtube.com</div>
+              <div className='font-medium text-sm pb-2 whitespace-nowrap w-full overflow-hidden text-ellipsis'>React Js - Giriş</div>
+              <img className='rounded-xl' src="https://i9.ytimg.com/vi_webp/CRt1E2V1tdo/maxresdefault.webp?v=657ec0b5&sqp=CJDc_qwG&rs=AOn4CLC-sU7KMYRXmApYM5Thg0uxgEuxOQ" alt='alt'/>
+            </div>
+            </a>
+          </div>
+
+          <div className='p-3'>
+            <a className=' p-5' href="https://youtu.be/n8IDqeaPi8U" target='_blank'>
+            <div className='w-96 h-auto bg-rose-200 rounded-xl p-3'>
+              <img className='w-10 pb-2' src='https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png?20220706172052' alt='alt'/>
+              <div className='pb-2 text-xs font-medium'>youtube.com</div>
+              <div className='font-medium text-sm pb-2 whitespace-nowrap w-full overflow-hidden text-ellipsis'>React Js - Giriş</div>
+              <img className='rounded-xl' src="https://i9.ytimg.com/vi_webp/CRt1E2V1tdo/maxresdefault.webp?v=657ec0b5&sqp=CJDc_qwG&rs=AOn4CLC-sU7KMYRXmApYM5Thg0uxgEuxOQ" alt='alt'/>
+            </div>
+            </a>
+
+            <a className=' p-5' href="https://youtu.be/n8IDqeaPi8U" target='_blank'>
+            <div className='w-96 h-auto bg-rose-200 rounded-xl p-3'>
+              <img className='w-10 pb-2' src='https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png?20220706172052' alt='alt'/>
+              <div className='pb-2 text-xs font-medium'>youtube.com</div>
+              <div className='font-medium text-sm pb-2 whitespace-nowrap w-full overflow-hidden text-ellipsis'>React Js - Giriş</div>
+              <img className='rounded-xl' src="https://i9.ytimg.com/vi_webp/CRt1E2V1tdo/maxresdefault.webp?v=657ec0b5&sqp=CJDc_qwG&rs=AOn4CLC-sU7KMYRXmApYM5Thg0uxgEuxOQ" alt='alt'/>
+            </div>
+            </a>
+          </div>
+
+          </div>
+        </div>
+
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
