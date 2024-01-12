eada85d229cc60c989967d7efa8dbbccbcdb55c4
UfukDemrel <126552914+UfukDemrel@users.noreply.github.com>
2024-01-12 20:31:52 +0300

    index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,81 +23,76 @@ export default function Home() {
           </div>
         </div>
 
-        <div className=' flex-1 p-3 box-border text-black pt-16 pb-16'>
-          <div className='flex justify-center'>
-            <div className=' w-32 border-2 h-32 p-3 rounded-xl bg-rose-300 mr-10'>
-              <img className='w-8 pb-3' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png' alt='alt'/>
-              <div className=' text-xs text-white pb-3'>@ufukdemrel</div>
-              <div className=' bg-rose-500 p-1 text-xs rounded-lg w-fit font-medium text-white'>Follow</div>
-            </div>
-            
-            <a target='_blank' href="https://twitter.com/FrontJockey"> 
-            <div className=' w-32 border-2 h-32 p-3 rounded-xl bg-slate-400 mr-10'>
-              <img className='w-8 pb-3' src='https://about.twitter.com/content/dam/about-twitter/x/brand-toolkit/logo-black.png.twimg.1920.png' alt='alt'/>
-              <div className=' text-xs text-white pb-3'>@ufukdemrel</div>
-              <div className=' bg-slate-600 p-1 text-xs rounded-lg w-fit font-medium text-white'>Follow</div>
+        <div className=''>
+          <div className='container flex flex-wrap justify-evenly'>
+
+            <a target='_blank' href="https://twitter.com/FrontJockey" className='mb-10'> 
+              <div className=' w-44 border-2 h-44 p-5 rounded-xl bg-rose-100'>
+                <img className='w-11 rounded-lg' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png' alt='alt'/>
+                <div className=' text-sm text-black pt-5 pb-5'>@ufukdemrel</div>
+                <div className='bg-sky-500 pt-1 pb-1 pl-3 pr-3 text-sm rounded-lg w-fit font-semibold text-white'>Follow</div>
+              </div>
+            </a> 
+
+            <a target='_blank' href="https://twitter.com/FrontJockey" className='mb-10'> 
+              <div className=' w-44 border-2 h-44 p-5 rounded-xl bg-slate-300'>
+                <img className='w-11 rounded-lg' src='https://seeklogo.com/images/T/twitter-icon-square-logo-108D17D373-seeklogo.com.png' alt='alt'/>
+                <div className=' text-sm text-black pt-5 pb-5'>@ufukdemrel</div>
+                <div className='bg-sky-500 pt-1 pb-1 pl-3 pr-3 text-sm rounded-lg w-fit font-semibold text-white'>Follow</div>
+              </div>
+            </a> 
+
+            <a target='_blank' href="https://www.linkedin.com/in/ufuk-demirel-3136b6183/" className='mb-10'> 
+            <div className=' w-44 border-2 h-44 p-5 rounded-xl bg-sky-400'>
+              <img className='w-11 rounded-lg' src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/480px-LinkedIn_logo_initials.png' alt='alt'/>
+              <div className='  text-sm text-black pt-5 pb-5'>@ufukdemrel</div>
+              <div className=' bg-sky-500 pt-1 pb-1 pl-3 pr-3 text-sm rounded-lg w-fit font-semibold text-white'>Follow</div>
             </div>
             </a>
 
-            <a target='_blank' href="https://www.linkedin.com/in/ufuk-demirel-3136b6183/"> 
-            <div className=' w-32 border-2 h-32 p-3 rounded-xl bg-sky-400 mr-10'>
-              <img className='w-8 pb-3' src='https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png' alt='alt'/>
-              <div className=' text-xs text-white pb-3'>@ufukdemrel</div>
-              <div className=' bg-sky-600 p-1 text-xs rounded-lg w-fit font-medium text-white'>Follow</div>
+            <a target='_blank' href="https://github.com/UfukDemrel" className='mb-10'> 
+            <div className=' w-44 border-2 h-44 p-5 rounded-xl '>
+              <img className='w-11 rounded-lg' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt='alt'/>
+              <div className=' text-sm text-black pt-5 pb-5'>@ufukdemrel</div>
+              <div className=' bg-black pt-1 pb-1 pl-3 pr-3 text-sm rounded-lg w-fit font-semibold text-white'>Follow</div>
             </div>
             </a>
-            
-            <a target='_blank' href="https://github.com/UfukDemrel"> 
-            <div className=' w-32 border-2 h-32 p-3 rounded-xl bg-slate-700 mr-10'>
-              <img className='w-8 pb-3' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt='alt'/>
-              <div className=' text-xs text-white pb-3'>@ufukdemrel</div>
-              <div className=' bg-slate-600 p-1 text-xs rounded-lg w-fit font-medium text-white'>Follow</div>
-            </div>
-            </a>
-          </div>
-          <div className='flex justify-center p-5'>
 
-          <div className='p-3'>
-            <a className=' p-5' href="https://youtu.be/n8IDqeaPi8U" target='_blank'>
-            <div className='w-96 h-auto bg-rose-200 rounded-xl p-3'>
+            <a target='_blank' href="https://youtu.be/n8IDqeaPi8U" className='mb-10'>
+            <div className='youtube bg-rose-100 rounded-xl p-5'>
               <img className='w-10 pb-2' src='https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png?20220706172052' alt='alt'/>
-              <div className='pb-2 text-xs font-medium'>youtube.com</div>
               <div className='font-medium text-sm pb-2 whitespace-nowrap w-full overflow-hidden text-ellipsis'>CodeEditor Clone Website - (Next.Js, React, TailwindCSS)</div>
-              <img className='rounded-xl' src="https://i9.ytimg.com/vi/n8IDqeaPi8U/maxresdefault.jpg?v=659ab0f4&sqp=CLjX_qwG&rs=AOn4CLDT2lG4tqa1WeHy1Es9FZlmjdQGwA" alt='alt'/>
+              <div className=' pb-16 text-xs font-medium'>youtube.com</div>
+              <img className='rounded-xl' src="https://i9.ytimg.com/vi/n8IDqeaPi8U/maxresdefault.jpg?v=659ab0f4&sqp=CJDgha0G&rs=AOn4CLABphjU5-ywr5E-TO-5qidxjnP6uw" alt='alt'/>
             </div>
             </a>
 
-            
-            <a className=' p-5' href="https://youtu.be/n8IDqeaPi8U" target='_blank'>
-            <div className='w-96 h-auto bg-rose-200 rounded-xl p-3'>
+            <a target='_blank' href="https://youtu.be/n8IDqeaPi8U" className='mb-10'>
+            <div className='youtube bg-rose-100 rounded-xl p-5'>
               <img className='w-10 pb-2' src='https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png?20220706172052' alt='alt'/>
-              <div className='pb-2 text-xs font-medium'>youtube.com</div>
               <div className='font-medium text-sm pb-2 whitespace-nowrap w-full overflow-hidden text-ellipsis'>React Js - Giriş</div>
-              <img className='rounded-xl' src="https://i9.ytimg.com/vi_webp/CRt1E2V1tdo/maxresdefault.webp?v=657ec0b5&sqp=CJDc_qwG&rs=AOn4CLC-sU7KMYRXmApYM5Thg0uxgEuxOQ" alt='alt'/>
+              <div className='pb-16 text-xs font-medium'>youtube.com</div>
+              <img className='rounded-xl' src="https://i9.ytimg.com/vi_webp/CRt1E2V1tdo/maxresdefault.webp?v=657ec0b5&sqp=CJDgha0G&rs=AOn4CLCIoa-QlzKKakmACyBcKXCzdha6KQ" alt='alt'/>
             </div>
             </a>
-          </div>
 
-          <div className='p-3'>
-            <a className=' p-5' href="https://youtu.be/n8IDqeaPi8U" target='_blank'>
-            <div className='w-96 h-auto bg-rose-200 rounded-xl p-3'>
+            <a target='_blank' href="https://youtu.be/uuHneKwjke8" className='mb-10'>
+            <div className='youtube bg-rose-100 rounded-xl p-5'>
               <img className='w-10 pb-2' src='https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png?20220706172052' alt='alt'/>
-              <div className='pb-2 text-xs font-medium'>youtube.com</div>
-              <div className='font-medium text-sm pb-2 whitespace-nowrap w-full overflow-hidden text-ellipsis'>React Js - Giriş</div>
-              <img className='rounded-xl' src="https://i9.ytimg.com/vi_webp/CRt1E2V1tdo/maxresdefault.webp?v=657ec0b5&sqp=CJDc_qwG&rs=AOn4CLC-sU7KMYRXmApYM5Thg0uxgEuxOQ" alt='alt'/>
+              <div className='font-medium text-sm pb-2 whitespace-nowrap w-full overflow-hidden text-ellipsis'>Next.Js - Giriş</div>
+              <div className='pb-16 text-xs font-medium'>youtube.com</div>
+              <img className='rounded-xl' src="https://i9.ytimg.com/vi/YFuT6rugwQQ/maxresdefault.jpg?v=6570a6fb&sqp=CJDgha0G&rs=AOn4CLAfNsZ5ZqHNtJ0K6HQZqNDGXSQbIA" alt='alt'/>
             </div>
             </a>
 
-            <a className=' p-5' href="https://youtu.be/n8IDqeaPi8U" target='_blank'>
-            <div className='w-96 h-auto bg-rose-200 rounded-xl p-3'>
+            <a target='_blank' href="https://youtu.be/Zkj5XrVAY1s" className='mb-10'>
+            <div className='youtube bg-rose-100 rounded-xl p-5'>
               <img className='w-10 pb-2' src='https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png?20220706172052' alt='alt'/>
-              <div className='pb-2 text-xs font-medium'>youtube.com</div>
-              <div className='font-medium text-sm pb-2 whitespace-nowrap w-full overflow-hidden text-ellipsis'>React Js - Giriş</div>
-              <img className='rounded-xl' src="https://i9.ytimg.com/vi_webp/CRt1E2V1tdo/maxresdefault.webp?v=657ec0b5&sqp=CJDc_qwG&rs=AOn4CLC-sU7KMYRXmApYM5Thg0uxgEuxOQ" alt='alt'/>
+              <div className='font-medium text-sm pb-2 whitespace-nowrap w-full overflow-hidden text-ellipsis'>Javascript - Giriş</div>
+              <div className='pb-16 text-xs font-medium'>youtube.com</div>
+              <img className='rounded-xl' src="https://i9.ytimg.com/vi/Zkj5XrVAY1s/maxresdefault.jpg?v=659d89f8&sqp=COzrha0G&rs=AOn4CLDLiEW6FP1en0wNP_B5MruEZr8FWg" alt='alt'/>
             </div>
             </a>
-          </div>
-
           </div>
         </div>
 
